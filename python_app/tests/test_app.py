652e9a826fbef6664706ba5b13f3663852fc3349
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-29 07:12:56 +0530

    problem scenario of sonarqube

--- python_app/tests/test_app.py
+++ python_app/tests/test_app.py
@@ -1,5 +1,8 @@
 import pytest
-from app import app
+from app import app, redis_client
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 @pytest.fixture
 def client():
